e4eafaa622d86197be35fa15ad7db1ee6ab5f456
Jayden Chan <jaydencn7@gmail.com>
2018-03-17 22:32:37 -0700

    Add polybar and edit i3 bar config

--- scripts/dots-up.sh
+++ scripts/dots-up.sh
@@ -3,3 +3,4 @@ cp    ~/.config/i3/config      ~/Documents/Git/dotfiles/i3
 cp -R ~/.vim/snippets          ~/Documents/Git/dotfiles/
 cp    ~/.config/termite/config ~/Documents/Git/dotfiles/termite
 cp    ~/.tmux.conf             ~/Documents/Git/dotfiles/tmux
+cp    ~/.config/polybar/config ~/Documents/Git/dotfiles/polybar
