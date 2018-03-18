ac0f8ace2dd7fd41cd127fed3569843352b9e023
Jayden Chan <jaydencn7@gmail.com>
2018-03-17 19:27:05 -0700

    Add dots download script

---
+++ scripts/dots-down.sh
@@ -0,0 +1,5 @@
+cp    ~/Documents/Git/dotfiles/compton/compton.conf ~/.config
+cp    ~/Documents/Git/dotfiles/i3/config            ~/.config/i3
+cp -R ~/Documents/Git/dotfiles/snippets             ~/.vim
+cp    ~/Documents/Git/dotfiles/termite/config       ~/.config/termite
+cp    ~/Documents/Git/dotfiles/tmux/.tmux.conf      ~/
