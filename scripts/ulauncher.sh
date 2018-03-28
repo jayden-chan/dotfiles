ebfd4d68c28627155a103842ae5d5a66f7261e06
Jayden Chan <jaydencn7@gmail.com>
2018-03-28 00:17:19 -0700

    Fix polybar and Ulauncher startup scripts

--- scripts/ulauncher.sh
+++ scripts/ulauncher.sh
@@ -3,8 +3,11 @@
 # Terminate already running instances
 killall -q ulauncher
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x ulauncher >/dev/null; do sleep 1; done
+# Wait for running instances to shut down
+while pgrep -u jayden -x ulauncher >/dev/null; do sleep 1; done
+
+# Wait until compton starts
+while ! pgrep -u jayden -x compton >/dev/null; do sleep 1; done
 
 # Launch Ulauncher
 ulauncher &
