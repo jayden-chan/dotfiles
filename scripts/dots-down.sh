68f5d43cec9fd3f18a1944be00097b353d0510d9
Jayden Chan <jaydencn7@gmail.com>
2018-03-29 23:20:05 -0700

    Misc changes after switching to Manjaro

--- scripts/dots-down.sh
+++ scripts/dots-down.sh
@@ -1,6 +1,6 @@
 cp    ~/Documents/Git/dotfiles/compton/compton.conf ~/.config
-cp    ~/Documents/Git/dotfiles/i3/config            ~/.config/i3
+cp    ~/Documents/Git/dotfiles/i3/config            ~/.i3
 cp -R ~/Documents/Git/dotfiles/snippets             ~/.vim
-cp    ~/Documents/Git/dotfiles/termite/config       ~/.config/termite
+cp    ~/Documents/Git/dotfiles/termite/config       ~/.config/termite/
 cp    ~/Documents/Git/dotfiles/tmux/.tmux.conf      ~/
 cp    ~/Documents/Git/dotfiles/polybar/config       ~/.config/polybar
--- scripts/dots-up.sh
+++ scripts/dots-up.sh
@@ -1,5 +1,5 @@
 cp    ~/.config/compton.conf   ~/Documents/Git/dotfiles/compton
-cp    ~/.config/i3/config      ~/Documents/Git/dotfiles/i3
+cp    ~/.i3/config             ~/Documents/Git/dotfiles/i3
 cp -R ~/.vim/snippets          ~/Documents/Git/dotfiles/
 cp    ~/.config/termite/config ~/Documents/Git/dotfiles/termite
 cp    ~/.tmux.conf             ~/Documents/Git/dotfiles/tmux
