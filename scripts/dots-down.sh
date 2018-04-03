4fd24c25e453c26754757abd1a136011075810e2
Jayden Chan <jaydencn7@gmail.com>
2018-04-02 21:49:10 -0700

    Properly set up vim spell file

--- scripts/dots-down.sh
+++ scripts/dots-down.sh
@@ -1,6 +1,7 @@
 cp    ~/Documents/Git/dotfiles/compton/compton.conf ~/.config
 cp    ~/Documents/Git/dotfiles/i3/config            ~/.config/i3/
 cp -R ~/Documents/Git/dotfiles/snippets             ~/.vim
+cp    ~/Documents/Git/dotfiles/vim/en.utf-8.add     ~/.vim
 cp    ~/Documents/Git/dotfiles/termite/config       ~/.config/termite/
 cp    ~/Documents/Git/dotfiles/tmux/.tmux.conf      ~/
 cp    ~/Documents/Git/dotfiles/polybar/config       ~/.config/polybar
