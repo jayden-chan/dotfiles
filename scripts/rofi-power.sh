746c14a981cb7eee96b6a6ae95519d0436ef0550
Jayden Chan <jaydencn7@gmail.com>
2018-04-03 23:14:45 -0700

    Add betterlockscreen to rofi power menu

--- scripts/rofi-power.sh
+++ scripts/rofi-power.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-RESULT=$(echo -e "logout\nreboot\nshutdown" | rofi -dmenu -i -p "power" -theme Custom-Nord -font "SF-Pro-Display 14" -lines 3 -width 25 -disable-history -tokenize)
+RESULT=$(echo -e "logout\nlock\nreboot\nshutdown" | rofi -dmenu -i -p "power" -theme Custom-Nord -font "SF-Pro-Display 14" -lines 4 -width 25 -disable-history -tokenize)
 
 if [ "$RESULT" = "logout" ];
 then
     i3-msg 'exit'
+elif [ "$RESULT" = "lock" ];
+then
+    exec betterlockscreen -l blur
 elif [ "$RESULT" = "reboot" ];
 then
     exec reboot
