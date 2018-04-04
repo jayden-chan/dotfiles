46210ce4cc45159e357c5cdb638450ffe49e013f
Jayden Chan <jaydencn7@gmail.com>
2018-04-03 23:33:22 -0700

    Remove Ulauncher and add notifications to scrot scripts

--- scripts/scrotS.sh
+++ scripts/scrotS.sh
@@ -3,3 +3,4 @@
 scrot -s ~/scrotTemp.png
 xclip -selection clipboard -t image/png -i ~/scrotTemp.png
 rm ~/scrotTemp.png
+notify-send Scrot Screenshot\ taken\ \(selection\)
--- scripts/scrotW.sh
+++ scripts/scrotW.sh
@@ -3,3 +3,4 @@
 scrot -u ~/scrotTemp.png
 xclip -selection clipboard -t image/png -i ~/scrotTemp.png
 rm ~/scrotTemp.png
+notify-send Scrot Screenshot\ taken\ \(window\)
