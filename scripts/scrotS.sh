1192d488d560213bb669f0f636173ad14991a7d6
Jayden Chan <jaydencn7@gmail.com>
2018-04-07 18:24:10 -0700

    Replace various 'home' references with $HOME

--- scripts/scrotS.sh
+++ scripts/scrotS.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-scrot -s ~/scrotTemp.png
-xclip -selection clipboard -t image/png -i ~/scrotTemp.png
-rm ~/scrotTemp.png
+scrot -s $HOME/scrotTemp.png
+xclip -selection clipboard -t image/png -i $HOME/scrotTemp.png
+rm $HOME/scrotTemp.png
 notify-send Scrot Screenshot\ taken\ \(selection\)
--- scripts/scrotW.sh
+++ scripts/scrotW.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-scrot -u ~/scrotTemp.png
-xclip -selection clipboard -t image/png -i ~/scrotTemp.png
-rm ~/scrotTemp.png
+scrot -u $HOME/scrotTemp.png
+xclip -selection clipboard -t image/png -i $HOME/scrotTemp.png
+rm $HOME/scrotTemp.png
 notify-send Scrot Screenshot\ taken\ \(window\)
