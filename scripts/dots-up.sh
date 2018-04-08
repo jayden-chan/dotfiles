7b9160a3214ff1b90cbc23e96f2a3ab19152c48e
Jayden Chan <jaydencn7@gmail.com>
2018-04-07 17:48:45 -0700

    Remove upload and download scripts in favor of symlinks

--- scripts/dots-up.sh
+++
@@ -1,8 +0,0 @@
-cp    ~/.config/compton.conf   ~/Documents/Git/dotfiles/compton
-cp    ~/.config/i3/config      ~/Documents/Git/dotfiles/i3
-cp -R ~/.vim/snippets          ~/Documents/Git/dotfiles/
-cp    ~/.vim/en.utf-8.add      ~/Documents/Git/dotfiles/vim
-cp    ~/.config/termite/config ~/Documents/Git/dotfiles/termite
-cp    ~/.tmux.conf             ~/Documents/Git/dotfiles/tmux
-cp    ~/.config/polybar/config ~/Documents/Git/dotfiles/polybar
-cp    ~/.config/dunst/dunstrc  ~/Documents/Git/dotfiles/dunst
