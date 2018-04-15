cdafcc425bc93285b7392cd723658caff30f527b
Jayden Chan <jaydencn7@gmail.com>
2018-04-15 01:19:40 -0700

    Add mouse accel for laptop touchpad

--- scripts/mouseaccel.sh
+++ scripts/mouseaccel.sh
@@ -4,4 +4,6 @@ for id in `xinput --list | grep 'pointer' | grep 'Logitech Gaming Mouse G502' | 
     xinput --set-prop $id 153 3.8 0.0 0.0 0.0 3.8 0.0 0.0 0.0 1.0
 done
 
+xinput --set-prop 'SYNA2B2C:01 06CB:7F27 Touchpad' 296 -0.2
+
 notify-send xinput 'Mouse sensitivity set'
