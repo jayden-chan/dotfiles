20d38ea37358c9e6510229df1b1928cef0d2e3c9
Jayden Chan <jaydencn7@gmail.com>
2018-06-22 13:44:25 -0700

    Misc changes after switching back to Arch

--- scripts/mouseaccel.sh
+++ scripts/mouseaccel.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/zsh
 
 for id in `xinput --list | grep 'pointer' | grep 'Logitech Gaming Mouse G502' | grep -o -P 'id=\d+' | grep -o -P '\d+'`; do
-    xinput --set-prop $id 155 3.8 0.0 0.0 0.0 3.8 0.0 0.0 0.0 1.0
-    xinput --set-prop $id 296 -1
+    xinput --set-prop $id 154 3.8 0.0 0.0 0.0 3.8 0.0 0.0 0.0 1.0
+    xinput --set-prop $id 295 -1
 done
 
 xinput --set-prop 'SYNA2B2C:01 06CB:7F27 Touchpad' 296 -0.2
