63c1f30a0b2031b186883bed7380dda42850728c
Jayden Chan <jaydencn7@gmail.com>
2018-07-22 00:17:58 -0700

    Add saved i3 layout, update polybar

---
+++ scripts/ide_layout.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/zsh
+
+i3-msg "workspace 4; layout splitv; gaps inner current set 0; gaps outer current set 46 append_layout /home/jayden/.config/i3/ide_layout.json; exec --no-startup-id termite"
+
+sleep 0.25
+
+i3-msg "focus down; exec --no-startup-id termite; focus up"
+
