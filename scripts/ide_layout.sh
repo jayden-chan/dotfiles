3959f2e8731d7ff0f57465cee24c28f5d4a99eb2
Jayden Chan <jaydencn7@gmail.com>
2018-08-05 21:10:55 -0700

    IDE layout script uses dotfiles file

--- scripts/ide_layout.sh
+++ scripts/ide_layout.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 
-i3-msg "workspace 4; layout splitv; gaps inner current set 0; gaps outer current set 46 append_layout /home/jayden/.config/i3/ide_layout.json; exec --no-startup-id termite"
+i3-msg "workspace 4; layout splitv; gaps inner current set 0; gaps outer current set 46 append_layout /home/jayden/Documents/Git/dotfiles/i3/ide_layout.json; exec --no-startup-id termite"
 
 sleep 0.25
 
