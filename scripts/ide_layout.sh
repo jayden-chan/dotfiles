d2965f5f14fa68cf4fe8aefd7fb158dfcbc4fe1c
Jayden Chan <jaydencn7@gmail.com>
2018-09-01 21:39:36 -0700

    Add new i3 layout & improve ide_layout script

--- scripts/ide_layout.sh
+++ scripts/ide_layout.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/zsh
 
-i3-msg "layout splitv; gaps inner current set 0; gaps outer current set 46 append_layout /home/jayden/Documents/Git/dotfiles/i3/ide_layout.json; exec --no-startup-id termite"
-
-sleep 0.25
-
-i3-msg "focus down; exec --no-startup-id termite; focus up"
-
+if [ $1 = "--single" ]; then
+    i3-msg "gaps inner current set 0; gaps outer current set 46 append_layout /home/jayden/Documents/Git/dotfiles/i3/ide1.json; exec --no-startup-id termite"
+    sleep 0.25
+    i3-msg "focus down; exec --no-startup-id termite"
+    sleep 0.25
+    i3-msg "focus up"
+elif [ $1 = "--double" ]; then
+    i3-msg "gaps inner current set 0; gaps outer current set 46 append_layout /home/jayden/Documents/Git/dotfiles/i3/ide2.json; exec --no-startup-id termite"
+    sleep 0.25
+    i3-msg "focus down; exec --no-startup-id termite"
+    sleep 0.25
+    i3-msg "focus right; exec --no-startup-id termite"
+    sleep 0.25
+    i3-msg "focus up"
+fi
