495be71350fc4512f5abeaa7c72712548e6fa329
Jayden Chan <jaydencn7@gmail.com>
2018-09-03 13:41:38 -0700

    Remove focus ups from ide_layout script

--- scripts/ide_layout.sh
+++ scripts/ide_layout.sh
@@ -4,14 +4,10 @@ if [ $1 = "--single" ]; then
     i3-msg "layout splitv; gaps inner current set 0; gaps outer current set 46 append_layout /home/jayden/Documents/Git/dotfiles/i3/ide1.json; exec --no-startup-id termite"
     sleep 0.25
     i3-msg "focus down; exec --no-startup-id termite"
-    sleep 0.25
-    i3-msg "focus up"
 elif [ $1 = "--double" ]; then
     i3-msg "gaps inner current set 0; gaps outer current set 46 append_layout /home/jayden/Documents/Git/dotfiles/i3/ide2.json; exec --no-startup-id termite"
     sleep 0.25
     i3-msg "focus down; exec --no-startup-id termite"
     sleep 0.25
     i3-msg "focus right; exec --no-startup-id termite"
-    sleep 0.25
-    i3-msg "focus up"
 fi
