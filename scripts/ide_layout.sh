09517d7bfad2a04834bd3180645223c75d401b3e
Jayden Chan <jaydencn7@gmail.com>
2018-09-03 13:42:38 -0700

    Add splitv to other layout script

--- scripts/ide_layout.sh
+++ scripts/ide_layout.sh
@@ -5,7 +5,7 @@ if [ $1 = "--single" ]; then
     sleep 0.25
     i3-msg "focus down; exec --no-startup-id termite"
 elif [ $1 = "--double" ]; then
-    i3-msg "gaps inner current set 0; gaps outer current set 46 append_layout /home/jayden/Documents/Git/dotfiles/i3/ide2.json; exec --no-startup-id termite"
+    i3-msg "layout splitv; gaps inner current set 0; gaps outer current set 46 append_layout /home/jayden/Documents/Git/dotfiles/i3/ide2.json; exec --no-startup-id termite"
     sleep 0.25
     i3-msg "focus down; exec --no-startup-id termite"
     sleep 0.25
