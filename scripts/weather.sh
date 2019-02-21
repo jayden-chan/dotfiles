cf1bd3b96f1a2ab2f1abad8e735c92dfe68c3248
Jayden Chan <jaydencn7@gmail.com>
2019-02-20 18:54:14 -0800

    Update weather script

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-weatherTemp=$(curl --silent "api.openweathermap.org/data/2.5/weather?q=Victoria,CA&appid=bdd632865ceed4ed50968235ef201e6a&units=metric" | jq -r ".main.temp")
+weatherTemp=$(curl --silent "api.openweathermap.org/data/2.5/weather?id=6174041&appid=bdd632865ceed4ed50968235ef201e6a&units=metric" | jq -r ".main.temp" | awk '{printf("%.0f\n", $1)}')
 
 echo $weatherTemp C
