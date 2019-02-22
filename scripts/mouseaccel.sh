701e68f8a366c0a02b637ab707f1791d174e25b7
Jayden Chan <jaydencn7@gmail.com>
2019-02-22 10:59:25 -0800

    Misc updates

--- scripts/mouseaccel.sh
+++ scripts/mouseaccel.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-for id in `xinput --list | grep 'pointer' | grep 'Logitech Gaming Mouse G502' | grep -o -P 'id=\d+' | grep -o -P '\d+'`; do
-    xinput --set-prop $id 155 3.8 0.0 0.0 0.0 3.8 0.0 0.0 0.0 1.0
-    xinput --set-prop $id 296 -1
-done
+if [ $(hostname) == "grace" ]; then
+    for id in `xinput --list | grep 'pointer' | grep 'Logitech Gaming Mouse G502' | grep -o -P 'id=\d+' | grep -o -P '\d+'`; do
+        xinput --set-prop $id 'Coordinate Transformation Matrix' 3.8 0.0 0.0 0.0 3.8 0.0 0.0 0.0 1.0
+        xinput --set-prop $id 'libinput Accel Speed' -1
+    done
+else
+    xinput --set-prop 'SYNA2B2C:01 06CB:7F27 Touchpad' 'libinput Natural Scrolling Enabled' 0
 
-xinput --set-prop 'SYNA2B2C:01 06CB:7F27 Touchpad' 286 1
+    xinput --set-prop 'Logitech USB Optical Mouse' 'Coordinate Transformation Matrix' 1.8 0.0 0.0 0.0 1.8 0.0 0.0 0.0 1.0
+    xinput --set-prop 'Logitech USB Optical Mouse' 'libinput Accel Speed' -1
+fi
