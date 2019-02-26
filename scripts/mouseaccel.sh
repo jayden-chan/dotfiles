c85993f031e7623125ffcd475cd0c193d10fb3c9
Jayden Chan <jaydencn7@gmail.com>
2019-02-26 09:07:07 -0800

    Update mouseaccel script for new mouse

--- scripts/mouseaccel.sh
+++ scripts/mouseaccel.sh
@@ -9,6 +9,8 @@ else
     xinput --set-prop 'SYNA2B2C:01 06CB:7F27 Touchpad' 'libinput Natural Scrolling Enabled' 0
     xinput --set-prop 'SYNA2B2C:01 06CB:7F27 Touchpad' 'libinput Tapping Enabled' 1
 
-    xinput --set-prop 'Logitech USB Optical Mouse' 'Coordinate Transformation Matrix' 1.8 0.0 0.0 0.0 1.8 0.0 0.0 0.0 1.0
-    xinput --set-prop 'Logitech USB Optical Mouse' 'libinput Accel Speed' -1
+    for id in `xinput --list | grep 'pointer' | grep 'Logitech MX Master 2S' | grep -o -P 'id=\d+' | grep -o -P '\d+'`; do
+        xinput --set-prop $id 'Coordinate Transformation Matrix' 2.0 0.0 0.0 0.0 2.0 0.0 0.0 0.0 1.0
+        xinput --set-prop $id 'libinput Accel Speed' -1
+    done
 fi
