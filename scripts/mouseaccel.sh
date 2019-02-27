4310d4951484c443ace01951884590441a06703a
Jayden Chan <jaydencn7@gmail.com>
2019-02-27 11:49:37 -0800

    Misc minor changes

--- scripts/mouseaccel.sh
+++ scripts/mouseaccel.sh
@@ -10,7 +10,7 @@ else
     xinput --set-prop 'SYNA2B2C:01 06CB:7F27 Touchpad' 'libinput Tapping Enabled' 1
 
     for id in `xinput --list | grep 'pointer' | grep 'Logitech MX Master 2S' | grep -o -P 'id=\d+' | grep -o -P '\d+'`; do
-        xinput --set-prop $id 'Coordinate Transformation Matrix' 2.0 0.0 0.0 0.0 2.0 0.0 0.0 0.0 1.0
+        xinput --set-prop $id 'Coordinate Transformation Matrix' 1.9 0.0 0.0 0.0 1.9 0.0 0.0 0.0 1.0
         xinput --set-prop $id 'libinput Accel Speed' -1
     done
 fi
