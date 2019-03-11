d65236a0a93d8fdf83f950fcf189b07689b69c6d
Jayden Chan <jaydencn7@gmail.com>
2019-03-10 17:39:10 -0700

    Updates for eslint, rofi, polybar

--- scripts/rofi.sh
+++ scripts/rofi.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ "$1" = "--normal" ]; then
-    rofi -show drun -display-drun "run:" -show-icons -drun-icon-theme "Papirus" -theme theme -font "Noto Sans 14" -lines 5 -width 50 -scroll-method 1
+    rofi -show drun -display-drun "run:" -show-icons -drun-icon-theme "Papirus" -theme theme -font "Nimbus Sans 14" -lines 5 -width 50 -scroll-method 1
 elif [ "$1" = "--power" ]; then
-    RESULT=$(echo -e "logout\nlock\nreboot\nshutdown" | rofi -dmenu -i -p "power:" -theme theme -font "SF-Pro-Display 14" -lines 4 -width 25 -disable-history -tokenize)
+    RESULT=$(echo -e "logout\nlock\nreboot\nshutdown" | rofi -dmenu -i -p "power:" -theme theme -font "Nimbus Sans 14" -lines 4 -width 25 -disable-history -tokenize)
 
     if [ "$RESULT" = "logout" ]; then
         i3-msg 'exit'
