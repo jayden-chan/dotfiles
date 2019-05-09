9851dd9783d8a1f7089a2aca4b2041c11b629b34
Jayden Chan <jaydencn7@gmail.com>
2019-05-09 11:57:36 -0700

    JC: Minor update to mouse accel script

--- scripts/mouseaccel.sh
+++ scripts/mouseaccel.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $(hostname) = "grace" ]; then
-    for id in `xinput --list | grep 'pointer' | grep 'Logitech Gaming Mouse G502' | grep -o -P 'id=\d+' | grep -o -P '\d+'`; do
+    for id in `xinput --list | rg 'Logitech Gaming Mouse G502\s+id=(\d+)\s+\[slave\s+pointer' --only-matching --replace='$1'`; do
         xinput --set-prop $id 'Coordinate Transformation Matrix' 3.8 0.0 0.0 0.0 3.8 0.0 0.0 0.0 1.0
         xinput --set-prop $id 'libinput Accel Speed' -1
     done
@@ -9,7 +9,7 @@ else
     xinput --set-prop 'SYNA2B2C:01 06CB:7F27 Touchpad' 'libinput Natural Scrolling Enabled' 0
     xinput --set-prop 'SYNA2B2C:01 06CB:7F27 Touchpad' 'libinput Tapping Enabled' 1
 
-    for id in `xinput --list | grep 'pointer' | grep 'Logitech MX Master 2S' | grep -o -P 'id=\d+' | grep -o -P '\d+'`; do
+    for id in `xinput --list | rg 'Logitech MX Master 2S\s+id=(\d+)\s+\[slave\s+pointer' --only-matching --replace='$1'`; do
         xinput --set-prop $id 'Coordinate Transformation Matrix' 1.6 0.0 0.0 0.0 1.6 0.0 0.0 0.0 1.0
         xinput --set-prop $id 'libinput Accel Speed' -1
     done
