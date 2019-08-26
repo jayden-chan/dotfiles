0d7d0794fcc7ee2340e03777f1a0b24fb24b3334
Jayden Chan <jaydencn7@gmail.com>
2019-08-26 11:07:43 -0600

    JC: Update mouseaccel script

--- scripts/mouseaccel.sh
+++ scripts/mouseaccel.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 if [ $(hostname) = "grace" ]; then
+    gspeed=3.8
     for id in `xinput --list | rg 'Logitech Gaming Mouse G502\s+id=(\d+)\s+\[slave\s+pointer' --only-matching --replace='$1'`; do
-        xinput --set-prop $id 'Coordinate Transformation Matrix' 3.8 0.0 0.0 0.0 3.8 0.0 0.0 0.0 1.0
+        xinput --set-prop $id 'Coordinate Transformation Matrix' $gspeed 0.0 0.0 0.0 $gspeed 0.0 0.0 0.0 1.0
         xinput --set-prop $id 'libinput Accel Speed' -1
     done
 else
     xinput --set-prop 'SYNA2B2C:01 06CB:7F27 Touchpad' 'libinput Natural Scrolling Enabled' 0
     xinput --set-prop 'SYNA2B2C:01 06CB:7F27 Touchpad' 'libinput Tapping Enabled' 1
 
-    for id in `xinput --list | rg 'Logitech MX Master 2S\s+id=(\d+)\s+\[slave\s+pointer' --only-matching --replace='$1'`; do
-        xinput --set-prop $id 'Coordinate Transformation Matrix' 1.6 0.0 0.0 0.0 1.6 0.0 0.0 0.0 1.0
-        xinput --set-prop $id 'libinput Accel Speed' -1
-    done
+    hspeed=1.8
+    xinput --set-prop 'HP HP Link-5 micro dongle Mouse' 'Coordinate Transformation Matrix' $hspeed 0.0 0.0 0.0 $hspeed 0.0 0.0 0.0 1.0
+    xinput --set-prop 'HP HP Link-5 micro dongle Mouse' 'libinput Accel Speed' -1
 fi
