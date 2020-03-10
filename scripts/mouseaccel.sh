3c7f18cc6e256a52e7d12befd3e2b03015012119
Jayden Chan <jaydencn7@gmail.com>
2020-03-09 22:11:42 -0700

    JC: Add coc-settings, update mouseaccel script

--- scripts/mouseaccel.sh
+++ scripts/mouseaccel.sh
@@ -10,6 +10,10 @@ if [ $(hostname) = "grace" ]; then
     cspeed=1.5
     xinput --set-prop 'Cooler Master Technology Inc. MM710 Gaming Mouse' 'Coordinate Transformation Matrix' $cspeed 0.0 0.0 0.0 $cspeed 0.0 0.0 0.0 1.0
     xinput --set-prop 'Cooler Master Technology Inc. MM710 Gaming Mouse' 'libinput Accel Speed' -1
+
+    bspeed=1.8
+    xinput --set-prop 'Telink Wireless Receiver Mouse' 'Coordinate Transformation Matrix' $bspeed 0.0 0.0 0.0 $bspeed 0.0 0.0 0.0 1.0
+    xinput --set-prop 'Telink Wireless Receiver Mouse' 'libinput Accel Speed' -1
 else
     xinput --set-prop 'SYNA2B2C:01 06CB:7F27 Touchpad' 'libinput Natural Scrolling Enabled' 0
     xinput --set-prop 'SYNA2B2C:01 06CB:7F27 Touchpad' 'libinput Tapping Enabled' 1
@@ -17,4 +21,8 @@ else
     hspeed=1.8
     xinput --set-prop 'HP HP Link-5 micro dongle Mouse' 'Coordinate Transformation Matrix' $hspeed 0.0 0.0 0.0 $hspeed 0.0 0.0 0.0 1.0
     xinput --set-prop 'HP HP Link-5 micro dongle Mouse' 'libinput Accel Speed' -1
+
+    bspeed=1.8
+    xinput --set-prop 'Telink Wireless Receiver Mouse' 'Coordinate Transformation Matrix' $bspeed 0.0 0.0 0.0 $bspeed 0.0 0.0 0.0 1.0
+    xinput --set-prop 'Telink Wireless Receiver Mouse' 'libinput Accel Speed' -1
 fi
