b3e6a24c4de20d17f91a5147294075a4dc5f57db
Jayden Chan <jaydencn7@gmail.com>
2020-07-08 14:48:54 -0600

    JC: Clean up deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,35 +1,36 @@
 #!/bin/sh
 
-rm -rf $HOME/.config/picom.conf   2>/dev/null
-rm -rf $HOME/.config/rofi         2>/dev/null
-rm -rf $HOME/.config/nvim         2>/dev/null
-rm -rf $HOME/.vim                 2>/dev/null
-rm -rf $HOME/.config/kitty        2>/dev/null
-rm -rf $HOME/.config/systemd/     2>/dev/null
+dots=$HOME/Documents/Git/dotfiles
+config=$HOME/.config
 
-mkdir -p $HOME/.config/nvim/colors
-mkdir -p $HOME/.config/nvim/spell
-mkdir -p $HOME/.config/rofi
-mkdir -p $HOME/.config/kitty
-mkdir -p $HOME/.config/systemd/user
+rm -rf $config/rofi       2>/dev/null
+rm -rf $config/nvim       2>/dev/null
+rm -rf $config/kitty      2>/dev/null
+rm -rf $config/systemd    2>/dev/null
+
+mkdir -p $config/rofi
+mkdir -p $config/nvim/colors
+mkdir -p $config/nvim/spell
+mkdir -p $config/kitty
+mkdir -p $config/systemd/user
 
 cd $HOME
-ln -fs $HOME/Documents/Git/dotfiles/picom/picom.conf        $HOME/.config/picom.conf
-ln -fs $HOME/Documents/Git/dotfiles/git/gitconfig           $HOME/.gitconfig
-ln -fs $HOME/Documents/Git/dotfiles/latex/latexmkrc         $HOME/.latexmkrc
-ln -fs $HOME/Documents/Git/dotfiles/rofi/theme.rasi         $HOME/.config/rofi/theme.rasi
-ln -fs $HOME/Documents/Git/dotfiles/snippets/               $HOME/.config/nvim/
-ln -fs $HOME/Documents/Git/dotfiles/vim/en.utf-8.add        $HOME/.config/nvim/spell/en.utf-8.add
-ln -fs $HOME/Documents/Git/dotfiles/vim/vimrc               $HOME/.config/nvim/init.vim
-ln -fs $HOME/Documents/Git/dotfiles/vscode/settings.json    $HOME/.config/Code/User/settings.json
-ln -fs $HOME/Documents/Git/dotfiles/vscode/keybindings.json $HOME/.config/Code/User/keybindings.json
-ln -fs $HOME/Documents/Git/dotfiles/zsh/zshrc               $HOME/.zshrc
-ln -fs $HOME/Documents/Git/dotfiles/zsh/zlogout             $HOME/.zlogout
-ln -fs $HOME/Documents/Git/dotfiles/awesome/theme.lua       $HOME/.config/awesome/themes/rainbow/theme.lua
-ln -fs $HOME/Documents/Git/dotfiles/awesome/rc.lua          $HOME/.config/awesome/rc.lua
-ln -fs $HOME/Documents/Git/dotfiles/kitty/kitty.conf        $HOME/.config/kitty/kitty.conf
-ln -fs $HOME/Documents/Git/dotfiles/starship/starship.toml  $HOME/.config/starship.toml
+ln -fs $dots/picom/picom.conf        $config/picom.conf
+ln -fs $dots/git/gitconfig           $HOME/.gitconfig
+ln -fs $dots/latex/latexmkrc         $HOME/.latexmkrc
+ln -fs $dots/rofi/theme.rasi         $config/rofi/theme.rasi
+ln -fs $dots/snippets/               $config/nvim/
+ln -fs $dots/vim/en.utf-8.add        $config/nvim/spell/en.utf-8.add
+ln -fs $dots/vim/vimrc               $config/nvim/init.vim
+ln -fs $dots/vscode/settings.json    $config/Code/User/settings.json
+ln -fs $dots/vscode/keybindings.json $config/Code/User/keybindings.json
+ln -fs $dots/zsh/zshrc               $HOME/.zshrc
+ln -fs $dots/zsh/zlogout             $HOME/.zlogout
+ln -fs $dots/awesome/theme.lua       $config/awesome/themes/rainbow/theme.lua
+ln -fs $dots/awesome/rc.lua          $config/awesome/rc.lua
+ln -fs $dots/kitty/kitty.conf        $config/kitty/kitty.conf
+ln -fs $dots/starship/starship.toml  $config/starship.toml
 
 if [ $(hostname) = "grace" ]; then
-    ln -fs $HOME/Documents/Git/dotfiles/systemctl/hue.service $HOME/.config/systemd/user/hue.service
+    ln -fs $dots/systemctl/hue.service $config/systemd/user/hue.service
 fi
