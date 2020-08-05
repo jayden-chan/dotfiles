d64cbd37fea3d89969ac4188c6b345d4d3baf47e
Jayden Chan <jaydencn7@gmail.com>
2020-08-05 16:02:12 -0600

    JC: Add TwitchTV script

---
+++ scripts/ttv.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+set -e
+
+url=""
+if [ -z "$1" ]; then
+	url=$(xclip -out -selection clipboard)
+else
+	url="https://twitch.tv/$1"
+fi
+
+streamlink --player "vlc --fullscreen" $url best
