a1683e998134eaf529451efd813a70d9ddd26841
Jayden Chan <jaydencn7@gmail.com>
2021-01-13 14:45:22 -0700

    JC: Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,6 +14,7 @@ mkdir -p $config/nvim/spell
 mkdir -p $config/kitty
 mkdir -p $config/systemd/user
 mkdir -p $config/awesome
+mkdir -p $HOME/.local/bin
 
 echo "Installing vim-plugged"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
@@ -31,22 +32,23 @@ else
 fi
 
 cd $HOME
-ln -fs $dots/git/gitconfig             $HOME/.gitconfig
-ln -fs $dots/latex/latexmkrc           $HOME/.latexmkrc
-ln -fs $dots/zsh/zshrc                 $HOME/.zshrc
-ln -fs $dots/zsh/zlogout               $HOME/.zlogout
-ln -fs $dots/tmux/tmux.conf            $HOME/.tmux.conf
-ln -fs $dots/rofi/theme.rasi           $config/rofi/theme.rasi
-ln -fs $dots/snippets/                 $config/nvim/UltiSnips
-ln -fs $dots/vim/en.utf-8.add          $config/nvim/spell/en.utf-8.add
-ln -fs $dots/vim/vimrc                 $config/nvim/init.vim
-ln -fs $dots/vscode/settings.json      $config/Code/User/settings.json
-ln -fs $dots/vscode/keybindings.json   $config/Code/User/keybindings.json
-ln -fs $dots/awesome/theme.lua         $config/awesome/themes/rainbow/theme.lua
-ln -fs $dots/awesome/awesome.png       $config/awesome/themes/rainbow/icons/awesome.png
-ln -fs $dots/awesome/rc.lua            $config/awesome/rc.lua
-ln -fs $dots/picom/picom.conf          $config/picom.conf
-ln -fs $dots/starship/starship.toml    $config/starship.toml
-ln -fs $dots/termite/config            $config/termite/config
+ln -fs $dots/awesome/awesome.png     $config/awesome/themes/rainbow/icons/awesome.png
+ln -fs $dots/awesome/rc.lua          $config/awesome/rc.lua
+ln -fs $dots/awesome/theme.lua       $config/awesome/themes/rainbow/theme.lua
+ln -fs $dots/git/gitconfig           $HOME/.gitconfig
+ln -fs $dots/latex/latexmkrc         $HOME/.latexmkrc
+ln -fs $dots/picom/picom.conf        $config/picom.conf
+ln -fs $dots/rofi/theme.rasi         $config/rofi/theme.rasi
+ln -fs $dots/scripts/p.js            $HOME/.local/bin/p
+ln -fs $dots/snippets/               $config/nvim/UltiSnips
+ln -fs $dots/starship/starship.toml  $config/starship.toml
+ln -fs $dots/termite/config          $config/termite/config
+ln -fs $dots/tmux/tmux.conf          $HOME/.tmux.conf
+ln -fs $dots/vim/en.utf-8.add        $config/nvim/spell/en.utf-8.add
+ln -fs $dots/vim/vimrc               $config/nvim/init.vim
+ln -fs $dots/vscode/keybindings.json $config/Code/User/keybindings.json
+ln -fs $dots/vscode/settings.json    $config/Code/User/settings.json
+ln -fs $dots/zsh/zlogout             $HOME/.zlogout
+ln -fs $dots/zsh/zshrc               $HOME/.zshrc
 
 echo "Finished deployment"
