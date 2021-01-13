1935e74767b0189a6c9088e235438d385e92c88d
Jayden Chan <jaydencn7@gmail.com>
2021-01-12 17:00:53 -0700

    JC: Remove unused scripts

--- scripts/keyboard.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-XAUTHORITY=/home/jayden/.Xauthority HOME=/home/jayden DISPLAY=:0 xset r rate 270 35
--- scripts/weather.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-weatherTemp=$(curl --silent "api.openweathermap.org/data/2.5/weather?id=6174041&appid=bdd632865ceed4ed50968235ef201e6a&units=metric" | jq -r ".main.temp" | awk '{printf("%.0f\n", $1)}')
-
-echo $weatherTemp C
