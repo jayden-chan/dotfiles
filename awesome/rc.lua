4ed0b8a85dbe500c29048f32361bbab2cf9c1683
Jayden Chan <jaydencn7@gmail.com>
2021-05-16 13:34:24 -0700

    JC: More misc updates for awesomewm & ssh

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -290,13 +290,13 @@ globalkeys = gears.table.join(
 
     awful.key({ modkey,           }, "j",
         function ()
-            awful.client.focus.byidx( 1)
+            awful.client.focus.byidx(-1)
         end,
         {description = "focus next by index", group = "client"}
     ),
     awful.key({ modkey,           }, "l",
         function ()
-            awful.client.focus.byidx(-1)
+            awful.client.focus.byidx( 1)
         end,
         {description = "focus previous by index", group = "client"}
     ),
