34a46afe85f1293222a914d93db1cd665f900781
Jayden Chan <jaydencn7@gmail.com>
2021-05-17 14:22:12 -0700

    JC: Remove MM710 from mouseaccel list

--- scripts/mouseaccel.sh
+++ scripts/mouseaccel.sh
@@ -9,8 +9,6 @@ done
 
 if [ $(hostname) = "grace" ]; then
     cspeed=1.5
-    xinput --set-prop 'Cooler Master Technology Inc. MM710 Gaming Mouse' 'Coordinate Transformation Matrix' $cspeed 0.0 0.0 0.0 $cspeed 0.0 0.0 0.0 1.0
-    xinput --set-prop 'Cooler Master Technology Inc. MM710 Gaming Mouse' 'libinput Accel Speed' -1
     xinput --set-prop 'Glorious Model O' 'Coordinate Transformation Matrix' $cspeed 0.0 0.0 0.0 $cspeed 0.0 0.0 0.0 1.0
     xinput --set-prop 'Glorious Model O' 'libinput Accel Speed' -1
     xinput --set-prop 'Glorious Model D' 'Coordinate Transformation Matrix' $cspeed 0.0 0.0 0.0 $cspeed 0.0 0.0 0.0 1.0
