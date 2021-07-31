f91fe0a43b9994c32f6beca3e97631a65bd35536
Jayden Chan <jaydencn7@gmail.com>
2021-07-31 16:59:53 -0700

    JC: More tweaks to bspwm, sxhkd, polybar, weather

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -2,9 +2,12 @@
 
 [ -e "$HOME/.config/ENV" ] && . $HOME/.config/ENV
 
+# default city Victoria BC
+city=${WEATHER_CITY_ID:-6174041}
+browser=${BROWSER:-firefox}
+
+if [ "$1" = "--open" ]; then $browser https://openweathermap.org/city/$city; exit; fi
 if [ -z "$WEATHER_TOKEN" ]; then echo "No token"; exit 1; fi
 
-city=${WEATHER_CITY:-Victoria,CA}
-url="api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$WEATHER_TOKEN"
-
+url="api.openweathermap.org/data/2.5/weather?id=$city&units=metric&appid=$WEATHER_TOKEN"
 curl --silent "$url" | jq -r '"\(.weather[0].description), \(.main.feels_like | round)C"'
