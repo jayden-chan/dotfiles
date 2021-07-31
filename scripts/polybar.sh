6d4eb3b47963db553f8414db3b3316e918fda372
Jayden Chan <jaydencn7@gmail.com>
2021-07-30 17:30:52 -0700

    JC: Switch to bspwm

---
+++ scripts/polybar.sh
@@ -0,0 +1,15 @@
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+if [ "$(hostname)" = "swift" ]; then
+    echo "---" | tee -a /tmp/polybar1.log
+    polybar center 2>&1 | tee -a /tmp/polybar1.log & disown
+else
+    echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log /tmp/polybar3.log
+    MONITOR=HDMI-0  polybar base   2>&1 | tee -a /tmp/polybar1.log & disown
+    MONITOR=DP-0    polybar center 2>&1 | tee -a /tmp/polybar2.log & disown
+    MONITOR=DVI-D-0 polybar base   2>&1 | tee -a /tmp/polybar3.log & disown
+fi
---
+++ scripts/weather.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+[ -e "$HOME/.config/ENV" ] && source $HOME/.config/ENV
+
+if [ -z "$WEATHER_TOKEN" ]; then echo "No token"; exit 1; fi
+
+city=${WEATHER_CITY:-Victoria,CA}
+url="api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$WEATHER_TOKEN"
+
+curl --silent "$url" | jq -r '"\(.main.feels_like | round)C \(.weather[0].main)"'
