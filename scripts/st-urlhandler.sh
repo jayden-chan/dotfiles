76c9a4f3e393c703e966fec2da9404df180f7fd8
Jayden Chan <jaydencn7@gmail.com>
2021-08-12 19:22:50 -0700

    JC: Further improve st-urlhandler scripts

--- scripts/st-urlhandler.sh
+++ scripts/st-urlhandler.sh
@@ -3,14 +3,22 @@
 # Based on:
 # https://github.com/LukeSmithxyz/st/blob/0af4782a47cc1b0918bdc41fb61b1a5d358f75f6/st-urlhandler
 
-urlregex='(http|https|git|ftp|ftps|)://((\w+\.\w+)|(localhost))(:\d+)?[-a-zA-Z0-9/]+'
-urls=$(tr -d '\n' | rg --only-matching -N $urlregex | sort | uniq)
+urlregex='((http|https|git|ftp|ftps|)://((\w+\.\w+)|(localhost)|(\d+\.\d+\.\d+\.\d+))(:\d+)?[-=&?_a-zA-Z0-9/]+)'
+pathregex='((\s|^)~(/(\w|\d|-|_)+)+(\.(\w|\d)+)?)'
 
-[ -z "$urls" ] && exit 1
+# make the script work across multiple lines and
+# perform a /home/<user> -> ~ conversion
+input=$(tr -d '\n' | sed "s|$HOME|~|g")
 
 while getopts "oc" o; do case "${o}" in
-    o) chosen="$(echo "$urls" | rofi -dmenu -i -p "follow url:" -theme links)"
-       setsid xdg-open "$chosen" >/dev/null 2>&1 & ;;
-    c) echo "$urls" | rofi -dmenu -i -p "copy url:" -theme links | tr -d '\n' | xclip -selection clipboard ;;
+    o)
+        matches=$(echo "$input" | rg --only-matching -N $urlregex | sort | uniq)
+        [ -z "$matches" ] && exit 1
+        chosen="$(echo "$matches" | rofi -dmenu -i -p "follow url:" -theme links)"
+        setsid xdg-open "$chosen" >/dev/null 2>&1 & ;;
+    c)
+        matches=$(echo "$input" | rg --only-matching -N "$urlregex|$pathregex" | sort | uniq)
+        [ -z "$matches" ] && exit 1
+        echo "$matches" | rofi -dmenu -i -p "copy url:" -theme links | tr -d '\n' | xclip -selection clipboard ;;
     *) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
 esac done
