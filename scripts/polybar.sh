7c1247c787ee90919fb78fd1a5964a1574101e08
Jayden Chan <jaydencn7@gmail.com>
2021-08-12 20:03:18 -0700

    JC: Block-style polybar

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -14,7 +14,7 @@ if [ "$(hostname)" = "swift" ]; then
     polybar center 2>&1 | tee -a /tmp/polybar1.log & disown
 else
     echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log /tmp/polybar3.log
-    MONITOR=HDMI-0  polybar --config="$config_path" base   2>&1 | tee -a /tmp/polybar1.log & disown
+    MONITOR=HDMI-0  polybar --config="$config_path" left   2>&1 | tee -a /tmp/polybar1.log & disown
     MONITOR=DP-0    polybar --config="$config_path" center 2>&1 | tee -a /tmp/polybar2.log & disown
-    MONITOR=DVI-D-0 polybar --config="$config_path" base   2>&1 | tee -a /tmp/polybar3.log & disown
+    MONITOR=DVI-D-0 polybar --config="$config_path" right  2>&1 | tee -a /tmp/polybar3.log & disown
 fi
--- scripts/weather.sh
+++ scripts/weather.sh
@@ -10,4 +10,4 @@ if [ "$1" = "--open" ]; then $browser https://openweathermap.org/city/$city; exi
 if [ -z "$WEATHER_TOKEN" ]; then echo "No token"; exit 1; fi
 
 url="api.openweathermap.org/data/2.5/weather?id=$city&units=metric&appid=$WEATHER_TOKEN"
-curl --silent "$url" | jq -r '"  \(.weather[0].description), \(.main.feels_like | round)C"'
+curl --silent "$url" | jq -r '"\(.weather[0].description), \(.main.feels_like | round)C"'
