5cf67cd00f49e7a8e722ed8dfb735140f6b365a1
Jayden Chan <jaydencn7@gmail.com>
2021-08-24 21:41:59 -0700

    JC: $HOME instead of /home/jayden where possible

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -20,7 +20,7 @@ function vpn () {
     sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
     sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
     sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
-    sudo openvpn --config $1 --auth-user-pass /home/jayden/Documents/pia/pass.txt
+    sudo openvpn --config $1 --auth-user-pass $HOME/Documents/pia/pass.txt
 }
 
 function installfont () {
