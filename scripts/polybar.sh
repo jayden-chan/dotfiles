ce761feb572b11dccbbe7c325c93552eaddc446d
Jayden Chan <jaydencn7@gmail.com>
2021-09-12 10:51:28 -0700

    JC: Improve polybar start script

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -13,8 +13,14 @@ if [ "$(hostname)" = "swift" ]; then
     echo "---" | tee -a /tmp/polybar1.log
     MONITOR=eDP-1 polybar --config="$config_path" laptop 2>&1 | tee -a /tmp/polybar1.log & disown
 else
-    echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log /tmp/polybar3.log
-    MONITOR=HDMI-0  polybar --config="$config_path" left   2>&1 | tee -a /tmp/polybar1.log & disown
-    MONITOR=DP-0    polybar --config="$config_path" center 2>&1 | tee -a /tmp/polybar2.log & disown
-    MONITOR=DVI-D-0 polybar --config="$config_path" right  2>&1 | tee -a /tmp/polybar3.log & disown
+    monitors=($(polybar -M | cut -d ':' -f 1))
+    if [[ "${#monitors[@]}" = "1" ]]; then
+        echo "---" | tee -a /tmp/polybar1.log
+        MONITOR="${monitors[0]}" polybar --config="$config_path" center 2>&1 | tee -a /tmp/polybar1.log & disown
+    else
+        echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log /tmp/polybar3.log
+        MONITOR=HDMI-0  polybar --config="$config_path" left   2>&1 | tee -a /tmp/polybar1.log & disown
+        MONITOR=DP-0    polybar --config="$config_path" center 2>&1 | tee -a /tmp/polybar2.log & disown
+        MONITOR=DVI-D-0 polybar --config="$config_path" right  2>&1 | tee -a /tmp/polybar3.log & disown
+    fi
 fi
