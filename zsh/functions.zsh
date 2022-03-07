30a6d1ca7370860e9eb5d26876e1716690ced6fa
Jayden Chan <jaydencn7@gmail.com>
2022-03-06 22:11:41 -0800

    JC: Update setwall function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -44,13 +44,13 @@ function bwg () {
 }
 
 function setwall () {
-    chosen_wall=$(sudo fd . -e png -e jpg -e jpeg -e webp | sxiv - -t -o)
+    chosen_wall=$(sudo fd . ${1:-.} -e png -e jpg -e jpeg -e webp | sxiv - -t -o)
     if [ "$chosen_wall" = "" ]; then
         echo "no image chosen"
     else
-        nitrogen --set-zoom-fill --save "$chosen_wall" --head=0
-        nitrogen --set-zoom-fill --save "$chosen_wall" --head=1
-        nitrogen --set-zoom-fill --save "$chosen_wall" --head=2
+        nitrogen --set-zoom-fill --save "$chosen_wall" --head=0 2>/dev/null
+        nitrogen --set-zoom-fill --save "$chosen_wall" --head=1 2>/dev/null
+        nitrogen --set-zoom-fill --save "$chosen_wall" --head=2 2>/dev/null
         sudo cp "$chosen_wall" /usr/share/backgrounds/wall
     fi
 }
