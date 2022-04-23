0c414afa07063f712d9e04db444547e717d8de52
Jayden Chan <jaydencn7@gmail.com>
2022-04-23 00:15:22 -0700

    JC: Add indoor temperature to polybar

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -7,7 +7,10 @@ city=${WEATHER_CITY_ID:-6174041}
 browser=${BROWSER:-firefox}
 
 if [ "$1" = "--open" ]; then $browser https://openweathermap.org/city/$city; exit; fi
-if [ -z "$WEATHER_TOKEN" ]; then echo "No token"; exit 1; fi
+if [ -z "$WEATHER_TOKEN" ]; then echo "No weather token"; exit 1; fi
+if [ -z "$HA_TOKEN" ]; then echo "No HA token"; exit 1; fi
 
 url="api.openweathermap.org/data/2.5/weather?id=$city&units=metric&appid=$WEATHER_TOKEN"
-curl --silent "$url" | jq -r '"\(.weather[0].description), \(.main.feels_like | round)C"'
+weather=$(curl --silent "$url" | jq -r '"\(.weather[0].description), \(.main.feels_like | round)C"')
+indoor_temp=$(ha desk_temp)
+echo "$weather ($indoor_temp C)"
