d8a621565e5395e9f88441079d305023afe20884
Jayden Chan <jaydencn7@gmail.com>
2022-05-26 11:14:36 -0700

    JC: Theme pick keybind

--- scripts/xcolor.sh
+++ scripts/xcolor.sh
@@ -5,3 +5,7 @@ color=$(xcolor | tr -d '\n' | xclip -selection c -filter)
 convert -size 75x75 "xc:$color" "$tmp_file"
 notify-send -i "$tmp_file" -t 3000 "Color picker" "$color"
 rm "$tmp_file"
+
+if [ "$1" = "--theme-pick" ]; then
+    sed -i -E "s|export ([A-Z]{6})_COLOR=\"#\w{6}\"|export \1_COLOR=\"$color\"|g" ~/.config/ENV
+fi
