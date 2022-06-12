6bfa222cfd7911b4f885a265df2ae7ac0ca972cb
Jayden Chan <jaydencn7@gmail.com>
2022-06-11 17:47:08 -0700

    JC: Enable spell checking for treesitter langs

--- vim/lua/config/treesitter.lua
+++ vim/lua/config/treesitter.lua
@@ -1,8 +1,9 @@
+local cmd = vim.cmd
+
 ---            ---
 --- Treesitter ---
 ---            ---
-require('nvim-treesitter.configs').setup({
-  ensure_installed = {
+local treesitter_langs = {
     "bash",
     "c",
     "cmake",
@@ -21,46 +22,56 @@ require('nvim-treesitter.configs').setup({
     "tsx",
     "typescript",
     "yaml",
-  },
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  indent = {
-    enable = true,
-  },
-   incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm",
+}
+
+require('nvim-treesitter.configs').setup({
+    ensure_installed = treesitter_langs,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
     },
-  },
+    indent = {
+        enable = true,
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+        },
+    },
 })
 require('spellsitter').setup()
+
+-- We can enable spell checking for any language that has
+-- treesitter since it will only check the comments and not the code
+for i,v in ipairs(treesitter_langs) do
+    cmd('autocmd FileType ' .. v .. ' setlocal spell')
+end
+
 require('nvim-treesitter.configs').setup({
-  textobjects = {
-    swap = {
-      enable = true,
-      swap_next = {
-        ["<leader>a"] = "@parameter.inner",
-      },
-      swap_previous = {
-        ["<leader>A"] = "@parameter.inner",
-      },
+    textobjects = {
+        swap = {
+            enable = true,
+            swap_next = {
+                ["<leader>a"] = "@parameter.inner",
+            },
+            swap_previous = {
+                ["<leader>A"] = "@parameter.inner",
+            },
+        },
+        select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["af"] = "@function.outer",
+                ["hf"] = "@function.inner",
+                ["ab"] = "@block.outer",
+                ["hb"] = "@block.inner",
+            },
+        },
     },
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ["af"] = "@function.outer",
-        ["hf"] = "@function.inner",
-        ["ab"] = "@block.outer",
-        ["hb"] = "@block.inner",
-      },
-    },
-  },
 })
