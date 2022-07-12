d4342234d3b5a258ebfa0a7164edd9ac9a386098
Jayden Chan <jaydencn7@gmail.com>
2022-07-12 13:15:03 -0700

    JC: More frequent indoor temp refresh

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -16,5 +16,23 @@ else
 fi
 
 url="api.openweathermap.org/data/2.5/weather?id=$city&units=metric&appid=$WEATHER_TOKEN"
-weather=$(curl --silent "$url" | jq -r '"\(.weather[0].description), \(.main.feels_like | round)C"')
+weather_file="$XDG_CACHE_HOME/weather"
+curr_time=$(date +'%s')
+
+if [ -f "$weather_file" ]; then
+    cached_weather=$(cut -f1 < "$weather_file")
+    weather_time=$(cut -f2 < "$weather_file")
+    time_diff=$(echo "$curr_time - $weather_time" | bc -l)
+
+    if [ "$time_diff" -gt "600" ]; then
+        weather=$(curl --silent "$url" | jq -r '"\(.weather[0].description), \(.main.feels_like | round)C"')
+        echo "$weather	$curr_time" > "$weather_file"
+    else
+        weather="$cached_weather"
+    fi
+else
+    weather=$(curl --silent "$url" | jq -r '"\(.weather[0].description), \(.main.feels_like | round)C"')
+    echo "$weather	$curr_time" > "$weather_file"
+fi
+
 echo "$weather$indoor_temp"
