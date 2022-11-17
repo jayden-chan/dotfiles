48f01f4cd3699268f79c808fabc48893c0fae076
Jayden Chan <jaydencn7@gmail.com>
2022-11-17 12:37:52 -0800

    JC: Misc vim updates

--- vim/lua/config/lsp.lua
+++ vim/lua/config/lsp.lua
@@ -126,7 +126,7 @@ cmp.setup.cmdline(":", {
 })
 
 -- Setup lspconfig.
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require("lspconfig")
 local default_flags = { debounce_text_changes = 150 }
 local on_init = function(client)
