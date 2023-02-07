cfd2dbd4e8f3ffd759fd7f7f3d744c8ec6e4e492
Jayden Chan <jaydencn7@gmail.com>
2023-02-06 20:57:39 -0800

    JC: Un-comment lazy.nvim auto install code

--- vim/init.lua
+++ vim/init.lua
@@ -3,21 +3,6 @@ require("config.autocmd")
 require("config.bindings")
 require("config.iab")
 
-local lazypath = "/home/jayden/.local/share/nvim/lazy/lazy.nvim"
-local statpath = vim.loop.fs_stat(lazypath)
--- if not statpath then
--- 	vim.fn.system({
--- 		"git",
--- 		"clone",
--- 		"--filter=blob:none",
--- 		"https://github.com/folke/lazy.nvim.git",
--- 		"--branch=stable", -- latest stable release
--- 		lazypath,
--- 	})
--- end
-
-vim.opt.rtp:prepend(lazypath)
-
 vim.cmd("filetype on")
 require("config.plugins")
 vim.cmd("syntax enable")
