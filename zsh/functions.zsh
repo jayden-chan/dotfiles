84da51da27c1100eeb371dd84abd4305b33c4fdd
Jayden Chan <jayden@jayden.codes>
2023-06-26 17:23:30 -0700

    JC: Improve sc alias/function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -7,6 +7,7 @@ function pach       () { cat /var/log/pacman.log | rg -i 'installed|upgraded|rem
 function bwu        () { export BW_SESSION="$(bw unlock --raw)" && bw sync }
 function qrimg      () { qrencode -t png -r /dev/stdin -o /dev/stdout | convert - -interpolate Nearest -filter point -resize 1000% png:/dev/stdout }
 function toh264     () { ffmpeg -i "$1" -c:v libx264 -c:a copy "${1:r}_h264.mp4" }
+function sc         () { jq .scripts ${1:-package.json} }
 
 # Good compression/archive settings
 function compress () { tar c -I"xz -T 0 -7" -f $1.tar.xz $1 }
