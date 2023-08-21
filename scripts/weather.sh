bc21deb93932c79b69d188bb6fa4e78970797dfd
Jayden Chan <jayden@jayden.codes>
2023-08-21 00:40:01 -0700

    JC: Move weather fetching to own service

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -6,7 +6,6 @@
 city=${WEATHER_CITY_ID:-6174041}
 
 if [ "$1" = "--open" ]; then xdg-open https://openweathermap.org/city/"$city"; exit; fi
-if [ -z "$WEATHER_TOKEN" ]; then echo "No weather token"; exit 1; fi
 
 if [ -z "$HA_TOKEN" ]; then
     indoor_temp=""
@@ -17,32 +16,5 @@ else
     fi
 fi
 
-url="api.openweathermap.org/data/2.5/weather?id=$city&units=metric&appid=$WEATHER_TOKEN"
-weather_file="$XDG_CACHE_HOME/weather"
-curr_time=$(date +'%s')
-
-if [ -f "$weather_file" ]; then
-    cached_weather=$(cut -f1 < "$weather_file")
-    weather_time=$(cut -f2 < "$weather_file")
-    time_diff=$(echo "$curr_time - $weather_time" | bc -l)
-
-    if [ "$time_diff" -gt "600" ]; then
-        weather_response=$(curl curl --silent "$url")
-        weather_code=$?
-        if [ "$weather_code" = "0" ]; then
-            weather=$(echo "$weather_response" | jq -r '"\(.weather[0].description), \(.main.feels_like | round)C"')
-            echo "$weather	$curr_time" > "$weather_file"
-        fi
-    else
-        weather="$cached_weather"
-    fi
-else
-    weather_response=$(curl curl --silent "$url")
-    weather_code=$?
-    if [ "$weather_code" = "0" ]; then
-        weather=$(echo "$weather_response" | jq -r '"\(.weather[0].description), \(.main.feels_like | round)C"')
-        echo "$weather	$curr_time" > "$weather_file"
-    fi
-fi
-
+weather=$(curl --silent "https://cron.jayden.codes/weather.txt")
 echo "$weather$indoor_temp"
