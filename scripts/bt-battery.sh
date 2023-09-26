cf4489532df7d36ec58ac74ac1352a75c0546999
Jayden Chan <jayden@jayden.codes>
2023-09-26 15:21:06 -0600

    JC: Fix bt-battery script when bluetooth disabled

--- scripts/bt-battery.sh
+++ scripts/bt-battery.sh
@@ -1,5 +1,13 @@
 #!/bin/zsh
 
+
+# if bluetooth is off we need to exit early because otherwise
+# the bluetoothctl command hangs forever
+active="$(systemctl status bluetooth | rg 'Active: active')"
+if [ "$active" = "" ]; then
+    exit 0
+fi
+
 device=$(bluetoothctl devices Connected | rg "^Device (.*?) $1$" --only-matching --replace='$1' --color=never)
 if [ "$device" != "" ]; then
     bluetoothctl info "$device" | rg "Battery Percentage.*\((\d+)\)" --only-matching --replace='$1' --color=never
