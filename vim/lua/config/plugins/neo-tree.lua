c7f683573e249040a0e52e0d5970903e4eef01c6
Jayden Chan <jayden@jayden.codes>
2024-09-16 13:29:44 -0600

    JC: Fix breaking changes from nerd fonts 3.0

--- vim/lua/config/plugins/neo-tree.lua
+++ vim/lua/config/plugins/neo-tree.lua
@@ -41,7 +41,7 @@ return {
 				icon = {
 					folder_closed = "",
 					folder_open = "",
-					folder_empty = "ﰊ",
+					folder_empty = "󰜌",
 					-- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
 					-- then these will never be used.
 					default = "*",
@@ -62,11 +62,11 @@ return {
 						added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
 						modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
 						deleted = "✖", -- this can only be used in the git_status source
-						renamed = "", -- this can only be used in the git_status source
+						renamed = "󰁕", -- this can only be used in the git_status source
 						-- Status type
 						untracked = "",
 						ignored = "",
-						unstaged = "",
+						unstaged = "󰄱",
 						staged = "",
 						conflict = "",
 					},
