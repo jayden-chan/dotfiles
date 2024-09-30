27f8a9d3c160c9ee0c9162732b772ef4ed3312fe
Jayden Chan <jayden@jayden.codes>
2024-09-30 10:32:37 -0600

    JC: Move sudoers lecture to Nix

--- nix/common/lecture.nix
+++ nix/common/lecture.nix
@@ -1,9 +1,21 @@
-{ config-vars, ... }:
+{ pkgs, ... }:
 
 {
   # setup the sudoers lecture
   security.sudo.extraConfig = ''
     Defaults lecture = always
-    Defaults lecture_file = ${config-vars.dotfiles-dir}/misc/sudoers.lecture
+    Defaults lecture_file = ${
+      pkgs.writeTextFile {
+        name = "sudoers-lecture";
+        text = ''
+
+          [1m     [32m"Bee" careful    [34m__
+                 [32mwith sudo!    [34m// \
+                               \\_/ [33m//
+             [35m'''-.._.-'''-.._.. [33m-(||)(')
+                               ''''[0m
+        '';
+      }
+    }
   '';
 }
