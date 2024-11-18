62fadc1a3d8b1654c755eee653cfa06bc3f7eb35
Jayden Chan <jayden@jayden.codes>
2024-11-17 19:16:41 -0700

    JC: Fix indentation in mpv script files

--- nix/home/mpv/script-append-url.nix
+++ nix/home/mpv/script-append-url.nix
@@ -20,32 +20,32 @@
 
       --handles the subprocess response table and return clipboard if it was a success
       local function handleres(res, args, primary)
-      	if not res.error and res.status == 0 then
-      		return res.stdout
-      	else
-      		--if clipboard failed try primary selection
-      		if not primary then
-      			append(true)
-      			return nil
-      		end
-      		msg.error("There was an error getting clipboard: ")
-      		msg.error("  Status: " .. (res.status or ""))
-      		msg.error("  Error: " .. (res.error or ""))
-      		msg.error("  stdout: " .. (res.stdout or ""))
-      		msg.error("args: " .. utils.to_string(args))
-      		return nil
-      	end
+          if not res.error and res.status == 0 then
+              return res.stdout
+          else
+              --if clipboard failed try primary selection
+              if not primary then
+                  append(true)
+                  return nil
+              end
+              msg.error("There was an error getting clipboard: ")
+              msg.error("  Status: " .. (res.status or ""))
+              msg.error("  Error: " .. (res.error or ""))
+              msg.error("  stdout: " .. (res.stdout or ""))
+              msg.error("args: " .. utils.to_string(args))
+              return nil
+          end
       end
 
       --main function
       local function append()
-      	local args = { "xclip", "-selection", primary and "primary" or "clipboard", "-out" }
-      	local clipboard = handleres(utils.subprocess({ args = args, cancellable = false }), args, primary)
-      	if clipboard then
-      		mp.commandv("loadfile", clipboard, "append-play")
-      		mp.osd_message("URL appended: " .. clipboard)
-      		msg.info("URL appended: " .. clipboard)
-      	end
+          local args = { "xclip", "-selection", primary and "primary" or "clipboard", "-out" }
+          local clipboard = handleres(utils.subprocess({ args = args, cancellable = false }), args, primary)
+          if clipboard then
+              mp.commandv("loadfile", clipboard, "append-play")
+              mp.osd_message("URL appended: " .. clipboard)
+              msg.info("URL appended: " .. clipboard)
+          end
       end
 
       mp.add_key_binding("ctrl+v", "appendURL", append)
--- nix/home/mpv/script-cut-video.nix
+++ nix/home/mpv/script-cut-video.nix
@@ -9,67 +9,67 @@
       local end_pos = {}
 
       local function timestamp(duration)
-      	local hours = duration / 3600
-      	local minutes = duration % 3600 / 60
-      	local seconds = duration % 60
-      	return string.format("%02d:%02d:%02.03f", hours, minutes, seconds)
+          local hours = duration / 3600
+          local minutes = duration % 3600 / 60
+          local seconds = duration % 60
+          return string.format("%02d:%02d:%02.03f", hours, minutes, seconds)
       end
 
       local function add_mark()
-      	local pos = mp.get_property_number("time-pos")
-      	if #start_pos == #end_pos then
-      		table.insert(start_pos, pos)
-      		mp.osd_message(string.format("START: %s", timestamp(pos)))
-      	else
-      		table.insert(end_pos, pos)
-      		mp.osd_message(string.format("END: %s", timestamp(pos)))
-      	end
+          local pos = mp.get_property_number("time-pos")
+          if #start_pos == #end_pos then
+              table.insert(start_pos, pos)
+              mp.osd_message(string.format("START: %s", timestamp(pos)))
+          else
+              table.insert(end_pos, pos)
+              mp.osd_message(string.format("END: %s", timestamp(pos)))
+          end
       end
 
       local function shell_quote(input)
-      	if string.find(input, "'") then
-      		return ' "' .. input .. '" '
-      	else
-      		return " '" .. input .. "' "
-      	end
+          if string.find(input, "'") then
+              return ' "' .. input .. '" '
+          else
+              return " '" .. input .. "' "
+          end
       end
 
       local function clear_marks()
-      	start_pos = {}
-      	end_pos = {}
-      	mp.osd_message("Cleared marks")
+          start_pos = {}
+          end_pos = {}
+          mp.osd_message("Cleared marks")
       end
 
       local function create_clip()
-      	if #start_pos == 0 or #end_pos == 0 then
-      		mp.osd_message("Missing marks")
-      		return
-      	end
+          if #start_pos == 0 or #end_pos == 0 then
+              mp.osd_message("Missing marks")
+              return
+          end
 
-      	if #start_pos ~= #end_pos then
-      		mp.osd_message("Mismatched marks")
-      		return
-      	end
+          if #start_pos ~= #end_pos then
+              mp.osd_message("Mismatched marks")
+              return
+          end
 
-      	local input_path = mp.get_property("stream-path")
-      	local shell_cmd = "bun run $DOT/misc/cut_video.ts" .. shell_quote(input_path)
-      	for i, k in pairs(start_pos) do
-      		shell_cmd = shell_cmd .. shell_quote(k)
-      		shell_cmd = shell_cmd .. shell_quote(end_pos[i])
-      	end
+          local input_path = mp.get_property("stream-path")
+          local shell_cmd = "bun run $DOT/misc/cut_video.ts" .. shell_quote(input_path)
+          for i, k in pairs(start_pos) do
+              shell_cmd = shell_cmd .. shell_quote(k)
+              shell_cmd = shell_cmd .. shell_quote(end_pos[i])
+          end
 
-      	local cmd = { "zsh", "-c", shell_cmd }
+          local cmd = { "zsh", "-c", shell_cmd }
 
-      	mp.osd_message("Rendering clip...", 900)
-      	mp.set_property("pause", "yes")
+          mp.osd_message("Rendering clip...", 900)
+          mp.set_property("pause", "yes")
 
-      	mp.command_native_async({ name = "subprocess", args = cmd, capture_stdout = false }, function(success, _, error)
-      		if success == true then
-      			mp.osd_message("Rendering clip complete", 30)
-      		else
-      			mp.osd_message(string.format("Rendering failed: %s", error), 30)
-      		end
-      	end)
+          mp.command_native_async({ name = "subprocess", args = cmd, capture_stdout = false }, function(success, _, error)
+              if success == true then
+                  mp.osd_message("Rendering clip complete", 30)
+              else
+                  mp.osd_message(string.format("Rendering failed: %s", error), 30)
+              end
+          end)
       end
 
       mp.add_key_binding("c", add_mark)
